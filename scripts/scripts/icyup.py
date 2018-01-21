4ec057c6ac9b8689d1326fa27fca907ecfe1533b
icyphox <icyph0x@protonmail.com>
2018-01-21 16:08:52 +0530

    add icyup.py

---
+++ scripts/scripts/icyup.py
@@ -0,0 +1,18 @@
+import string
+import sys
+import os
+from random import *
+from subprocess import call
+
+# generate random string 
+st = string.ascii_letters + string.digits
+rand_st = ''.join(choice(st) for x in range(6))
+
+local_file = sys.argv[1]
+ext = os.path.splitext(local_file)[1]
+remote_file = 'upload/' + rand_st + ext
+
+scp_cmd = 'scp ' + local_file + ' boop:' + remote_file
+os.popen(scp_cmd)
+
+print('https://xix.ph0x.me/' + rand_st + ext)
