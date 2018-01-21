4af2e2b4c501b74eeaacc4a2e5bc7232bc918d71
icyphox <icyph0x@protonmail.com>
2018-01-21 19:14:22 +0530

    add grayscaling to lock script

--- scripts/scripts/lock.sh
+++ scripts/scripts/lock.sh
@@ -2,9 +2,9 @@
 
 image_file=/tmp/screen_lock.png
 resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
-filters='gblur=sigma=8'
+filters='gblur=sigma=15'
 ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 \
-  -vf "$filters" "$image_file"
-i3lock -i $image_file -l '#d8dee9' -o '#99C794' -w '#EC5f67'
+  -vf "$filters,format=gray" "$image_file" 
+i3lock -i $image_file -l '#222222' -o '#99C794' -w '#EC5f67'
 
 
