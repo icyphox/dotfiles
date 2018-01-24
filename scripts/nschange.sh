211879bfe6b63885ddb4fb51aa8fc7ecb66da18b
icyphox <icyph0x@protonmail.com>
2018-01-24 19:53:24 +0530

    scripts/nschange.sh is now a full-fledged pre game script

--- scripts/nschange.sh
+++ scripts/nschange.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# run as root
+# create a SOCKS proxy 
+ssh -D 8008 boop
 
-echo nameserver 8.8.8.8 > /etc/resolv.conf
-chattr +i /etc/resolv.conf
-systemctl stop dhcpcd.sevice
+# change nameservers
+sudo echo nameserver 8.8.8.8 > /etc/resolv.conf
+
+# write-lock the file (even root can't edit it)
+sudo chattr +i /etc/resolv.conf
+
+# kill dhcpcd (messes with IP)
+sudo systemctl stop dhcpcd.sevice
+
+# start VPN
+sudo openvpn --config ~/icy.ovpn
