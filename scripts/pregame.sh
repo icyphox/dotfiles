37bc4d4a912362c6b122a9744ee085f9fcc447b2
icyphox <icyph0x@protonmail.com>
2018-01-24 20:00:07 +0530

    can't ssh that way apparently

--- scripts/pregame.sh
+++ scripts/pregame.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-# create a SOCKS proxy 
-ssh -D 8008 boop
-
 # change nameservers
-sudo echo nameserver 8.8.8.8 > /etc/resolv.conf
+ echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 # write-lock the file (even root can't edit it)
-sudo chattr +i /etc/resolv.conf
+ chattr +i /etc/resolv.conf
 
 # kill dhcpcd (messes with IP)
-sudo systemctl stop dhcpcd.sevice
+ systemctl stop dhcpcd.sevice
 
 # start VPN
-sudo openvpn --config ~/icy.ovpn
+ openvpn --config ~/icy.ovpn
