98b090c1d2d900f4e5c5c4fba67da179854b8231
Anirudh <icyph0x@pm.me>
2018-06-27 10:33:14 +0530

    refactor(bin): icyup.sh now generates a random md5

--- bin/icyup.sh
+++ bin/icyup.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
+
+hash=`md5sum <<EOF
+$RANDOM
+EOF` 
+
+fn=`echo $hash | awk '// { print $1 }'`
 
 if [ "$1" != "" ]; then
-	file=$(basename $1)
-	scp $1 file boop:www/upload/
-	echo "https://xix.ph0x.me/$file"
+	echo $fn
+	scp $1 boop:www/upload/$fn
+	echo "https://xix.ph0x.me/$fn"
 else
 	echo "no path specified :v"
 fi
