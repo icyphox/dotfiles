c1381110abe293315b529da8b942a74ed681115e
Anirudh <icyph0x@pm.me>
2018-06-28 16:30:44 +0530

    refactor(bin): icyup.sh: md5 + extension

--- bin/icyup.sh
+++ bin/icyup.sh
@@ -7,9 +7,11 @@ EOF`
 fn=`echo $hash | awk '// { print $1 }'`
 
 if [ "$1" != "" ]; then
-	echo $fn
-	scp $1 boop:www/upload/$fn
-	echo "https://xix.ph0x.me/$fn"
+	file=$1
+	ext="${file##*.}"
+	fullname="$fn.$ext"
+	scp $1 boop:www/upload/$fullname
+	echo "https://xix.ph0x.me/$fullname"
 else
 	echo "no path specified :v"
 fi
