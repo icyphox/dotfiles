af9f99be0ed0f0d1a88fc8ee12bb0d4d3aaa9206
Anirudh <icyph0x@pm.me>
2019-03-17 09:11:05 +0530

    Update vpn script to revert to Uni DNS on exit
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- bin/vpnon.sh
+++ bin/vpnon.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+sudo cp /etc/resolv.conf /etc/resolv.conf.bk
 sudo cp ~/Dotfiles/resolv.conf /etc/resolv.conf
 sudo openvpn --config ~/client.ovpn --script-security 2
+sudo cp /etc/resolv.conf.bk /etc/resolv.conf
