a755fbc4e51746fbccfd67b6259b91ca169269f2
Anirudh <icyph0x@pm.me>
2019-06-12 16:22:54 +0530

    Configure i3lock and xautlock
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 image_file=/tmp/screen_lock.png
 resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
 filters='gblur=sigma=15'
 ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 \
   -vf "$filters" "$image_file" 
-i3lock -i $image_file -l '#222222' -o '#99C794' -w '#EC5f67'
-
-
+i3lock -i $image_file -l '#ffffff' -o '#99C794' -w '#EC5f67'
