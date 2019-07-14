f5ecdce0de8cfd894c15eb50de438a185a0098f5
Anirudh <icyph0x@pm.me>
2019-07-14 14:34:28 +0530

    VPN fixes
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- bin/redirectgateway-def1-socks.sh
+++ bin/redirectgateway-def1-socks.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$script_type" == "up" ]; then
-  #route add -host $remote_1/32 gw $route_net_gateway
-  #route add -net 0.0.0.0/1 gw $route_vpn_gateway
-  #route add -net 128.0.0.0/1 gw $route_vpn_gateway
+  route add -host $remote_1/32 gw $route_net_gateway
+  route add -net 0.0.0.0/1 gw $route_vpn_gateway
+  route add -net 128.0.0.0/1 gw $route_vpn_gateway
   /etc/openvpn/update-resolv-conf
 elif [ "$script_type" == "down" ]; then
   /etc/openvpn/update-resolv-conf
--- bin/vpnon.sh
+++ bin/vpnon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo cp /etc/resolv.conf /etc/resolv.conf.bk
-sudo cp ~/Dotfiles/resolv.conf /etc/resolv.conf
-sudo openvpn --config ~/client.ovpn --script-security 2
+sudo cp ~/dotfiles/resolv.conf /etc/resolv.conf
+sudo openvpn --config ~/client.ovpn
 sudo cp /etc/resolv.conf.bk /etc/resolv.conf
