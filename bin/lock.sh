8a042f473974e47d5a6cbbbd24a49583b00899a6
Anirudh <icyph0x@pm.me>
2019-07-18 17:55:55 +0530

    Darken lockscreen
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 image_file=/tmp/screen_lock.png
+import -window root "$image_file"
 resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
-filters='gblur=sigma=15'
+filters="avgblur,hue=s=0,eq=brightness=-0.09"
 ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 \
-  -vf "$filters" "$image_file" 
-i3lock -i $image_file -l '#666666' -o '#99C794' -w '#EC5f67'
+-vf "$filters" "$image_file"
+i3lock -i $image_file -l '#ffffff' -o '#99C794' -w '#EC5f67'
