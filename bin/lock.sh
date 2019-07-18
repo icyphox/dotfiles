04145ab9ba39f28c8c283b85b5f0f254d9ffb322
Anirudh <icyph0x@pm.me>
2019-07-18 18:48:05 +0530

    Wups wrong blur
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- bin/lock.sh
+++ bin/lock.sh
@@ -3,7 +3,7 @@
 image_file=/tmp/screen_lock.png
 import -window root "$image_file"
 resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
-filters="avgblur,hue=s=0,eq=brightness=-0.09"
+filters="gblur=sigma=15,hue=s=0,eq=brightness=-0.09"
 ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 \
 -vf "$filters" "$image_file"
 i3lock -i $image_file -l '#ffffff' -o '#99C794' -w '#EC5f67'
