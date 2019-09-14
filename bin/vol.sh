e127293d9be6179083d08c51c0578d72cf51b6af
Anirudh Oppiliappan <x@icyphox.sh>
2019-09-14 18:02:16 +0530

    bin/vol.sh: Indicate mute status
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- bin/vol.sh
+++ bin/vol.sh
@@ -14,8 +14,13 @@ do
 			amixer sset Master 2%-
 			;;
 		q)
-			cur_vol=$(amixer get Master | grep 'Right' | awk -F'[][]' '{ print $2 }' | tr -d '\n')
-			echo -ne "%{F$light}vol %{F$fg}$cur_vol"
+			if [[ $(amixer get Master | grep 'Right' | awk -F'[][] ' '{ print $2 }' | tr -d '\n') = "[on]" ]]
+			then
+				cur_vol=$(amixer get Master | grep 'Right' | awk -F'[][]' '{ print $2 }' | tr -d '\n')
+				echo -ne "%{F$light}vol %{F$fg}$cur_vol"
+			else
+				echo -ne "%{F$light}vol %{F$fg}muted"
+			fi
 			;;
 	esac
 done
