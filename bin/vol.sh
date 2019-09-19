0b498183321bfc3b89ed050fb638ad5b7c9ae21f
Anirudh Oppiliappan <x@icyphox.sh>
2019-09-19 21:34:08 +0530

    bin/vol.sh: Fix volume script
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- bin/vol.sh
+++ bin/vol.sh
@@ -14,9 +14,9 @@ do
 			amixer sset Master 2%-
 			;;
 		q)
-			if [[ $(amixer get Master | grep 'Right' | awk -F'[][] ' '{ print $2 }' | tr -d '\n') = "[on]" ]]
+			if [[ $(amixer get Master | awk -F"[][] " '{ print $3 }' | tr -d '\n') = "[on]" ]]
 			then
-				cur_vol=$(amixer get Master | grep 'Right' | awk -F'[][]' '{ print $2 }' | tr -d '\n')
+				cur_vol=$(amixer get Master | awk -F"[][]" '/dB/ { print $2 }' | tr -d '\n')
 				echo -ne "%{F$light}vol %{F$fg}$cur_vol"
 			else
 				echo -ne "%{F$light}vol %{F$fg}muted"
