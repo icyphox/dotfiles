2381a594f9a7764d1a068e5c4cbd7588ca38a6f0
Anirudh Oppiliappan <x@icyphox.sh>
2019-10-22 18:26:42 +0530

    Dunno
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- bin/vol.sh
+++ bin/vol.sh
@@ -14,12 +14,8 @@ do
 			pamixer -d 2
 			;;
 		q)
-			cur_vol=$(pamixer --get-volume-human)
-            if [[ "$cur_vol" == "muted" ]]; then
-				echo -ne "%{F$light}vol %{F$fg}muted"
-			else
-			    echo -ne "%{F$light}vol %{F$fg}$cur_vol"
-			fi
+            cur_vol=$(pamixer --get-volume-human)
+			echo -ne "%{F$light}vol %{F$fg}$cur_vol"
 			;;
 	esac
 done
