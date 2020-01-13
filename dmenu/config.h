7b11a72879acfe5c41286ca9dbb0533ad8643898
Anirudh Oppiliappan <x@icyphox.sh>
2020-01-13 10:00:20 +0000

    Add suckless stuff
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- dmenu/config.h
+++ dmenu/config.h
@@ -8,8 +8,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static unsigned int lineheight = 35;
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#383838", "#f8f8f8" },
-	[SchemeSel] = { "#f8f8f8", "#383838" },
+	[SchemeNorm] = { "#cbd6e2", "#121212" },
+	[SchemeSel] = { "#121212", "#cbd6e2" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
