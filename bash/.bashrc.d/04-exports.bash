54d62703aea39a26651cd35cb339724ff3845cee
Anirudh Oppiliappan <x@icyphox.sh>
2020-03-13 11:09:22 +0530

    bash: Add pgsql to PATH
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- bash/.bashrc.d/04-exports.bash
+++ bash/.bashrc.d/04-exports.bash
@@ -9,6 +9,7 @@ export INPUTRC=~/.inputrc
 export PATH=$PATH:$HOME/.nimble/bin
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/usr/local/pgsql/bin
 export PW_DIR=~/passwords
 export PW_KEY=x@icyphox.sh
 export _Z_CMD="j"
