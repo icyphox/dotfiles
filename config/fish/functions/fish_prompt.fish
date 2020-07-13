063efd2904acee6812dacc44181a07ad18400c9e
Anirudh Oppiliappan <x@icyphox.sh>
2020-07-13 20:39:56 +0530

    Have git prompt show clean/dirty state

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,12 +1,13 @@
 function fish_prompt --description 'Write out the prompt'
     set -l cyan (set_color cyan)
-    set -l prefix '▲'
+    set -l prefix '$'
 
     printf '\n'
     set_color cyan
     printf '%s' (prompt_pwd)
     set_color normal
-
-    printf '%s\n' (__fish_git_prompt)
+    printf '%s\n' (__fish_git_prompt)(__fish_git_prompt_dirty)
+    set_color white
     printf '%s ' $prefix
+    set_color normal
  end
