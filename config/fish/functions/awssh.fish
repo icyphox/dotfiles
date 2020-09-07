dd9f656317be3c3b1181a80d4cf6638666fac3af
Anirudh Oppiliappan <x@icyphox.sh>
2020-09-07 10:27:07 +0530

    More things

--- config/fish/functions/awssh.fish
+++ config/fish/functions/awssh.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.Gn4pMQ/awssh.fish @ line 2
+# Defined in /tmp/fish.luUeJj/awssh.fish @ line 2
 function awssh
-    ssh ubuntu@$1 -i ~/.ssh/cometchat-aws.pem
+    ssh ubuntu@$argv -i ~/.ssh/cometchat-aws.pem
 end
--- config/fish/functions/gc.fish
+++ config/fish/functions/gc.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.ybggTQ/gc.fish @ line 2
-function gc --wraps='git commit -v' --wraps='git commit -v' --description 'alias gc=git commit -v'
+# Defined in /tmp/fish.XtHOuP/gc.fish @ line 2
+function gc --wraps='git commit -v' --description 'alias gc=git commit -v'
   git commit -v $argv;
 end
