39dc6af4355d6b7dc39eb90f8a8d71e5f263fe3b
Anirudh Oppiliappan <x@icyphox.sh>
2020-11-19 10:59:17 +0530

    Things I thought I'd commited
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,9 @@
 set fish_greeting
 
 set -gx PATH ~/bin ~/.local/bin ~/leet/Nim/bin ~/.nimble/bin \
-/usr/local/bin /usr/bin /bin /sbin /usr/sbin /usr/X11R6/bin
+/usr/local/bin /usr/bin /bin /sbin /usr/sbin /usr/X11R6/bin \
+~/bin/wm
+
 set -gx EDITOR nvim
 set -gx BROWSER iridium
 set -gx PW_KEY x@icyphox.sh
