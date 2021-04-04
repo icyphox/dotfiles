0c2a974761e276ecfe4facc2be4ce4f588ea80be
Anirudh Oppiliappan <x@icyphox.sh>
2021-04-04 11:21:03 +0530

    Fix fish prompt after upgrade

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -6,7 +6,7 @@ function fish_prompt --description 'Write out the prompt'
     set_color cyan
     printf '%s' (prompt_pwd)
     set_color normal
-    printf '%s\n' (__fish_git_prompt)(__fish_git_prompt_dirty)
+    printf '%s\n' (fish_git_prompt)(__fish_git_prompt_informative_status)
     set_color white
     printf '%s ' $prefix
     set_color normal
