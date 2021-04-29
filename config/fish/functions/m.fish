53626d8490c0fe8744565d01f35b1203fc2c6524
Anirudh Oppiliappan <x@icyphox.sh>
2021-04-29 18:55:10 +0530

    Some things new in bin

--- config/fish/functions/m.fish
+++ config/fish/functions/m.fish
@@ -1,4 +1,9 @@
 # Defined in - @ line 1
 function m --wraps=nail --description 'alias m=nail'
-  nail  $argv;
+  switch (uname)
+    case OpenBSD
+      nail  $argv;
+	case Darwin
+	  s-nail $argv;
+  end
 end
