c00a824219a928e4c66dc5ad0b67abaef01dfc86
Anirudh Oppiliappan <x@icyphox.sh>
2021-07-22 09:40:43 +0530

    Wawa

--- config/nvim/ftplugin/markdown.vim
+++ config/nvim/ftplugin/markdown.vim
@@ -1,8 +1,3 @@
 set tw=72
 setlocal expandtab
-
-let g:vim_markdown_no_default_key_mappings=1
-let g:vim_markdown_toml_frontmatter=1
-let g:vim_markdown_yaml_frontmatter=1
-let g:vim_markdown_folding_disabled=1
-let g:vim_markdown_conceal=0
+set conceallevel=0
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,15 +1,16 @@
 vim.cmd 'packadd paq-nvim'
 require 'paq-nvim' {
- 'jiangmiao/auto-pairs';
- 'airblade/vim-gitgutter';
-{ 'vim/vim-colors-plain', url='https://git.peppe.rs' };
- 'tpope/vim-surround';
-{ 'vim/better-text-objs', url='https://git.peppe.rs' };
- 'wellle/targets.vim';
- 'tpope/vim-rsi';
- 'neovim/nvim-lspconfig';
- 'nvim-lua/completion-nvim';
- 'steelsojka/completion-buffers';
+  'hjson/vim-hjson';
+  'jiangmiao/auto-pairs';
+  'airblade/vim-gitgutter';
+  { 'vim/vim-colors-plain', url='https://git.peppe.rs' };
+  'tpope/vim-surround';
+  { 'vim/better-text-objs', url='https://git.peppe.rs' };
+  'wellle/targets.vim';
+  'tpope/vim-rsi';
+  'neovim/nvim-lspconfig';
+  'nvim-lua/completion-nvim';
+  'steelsojka/completion-buffers';
 }
 
 require('settings')
