e3a4571f657bd54f9e63954a89718485284fe311
Anirudh Oppiliappan <x@icyphox.sh>
2021-08-21 16:04:56 +0530

    Comment out prompt title trap

--- bash/.bashrc.d/99-prompt.bash
+++ bash/.bashrc.d/99-prompt.bash
@@ -2,5 +2,5 @@ PS1='$(~/dotfiles/prompt/prompt) '
 PS2="> "
 
 # terminal title
-trap 'printf "\033]2;$(history 1 | sed "s/^[ ]*[0-9]*[ ]*//g"): $PWD\007"' DEBUG
+# trap 'printf "\033]2;$(history 1 | sed "s/^[ ]*[0-9]*[ ]*//g"): $PWD\007"' DEBUG
 
