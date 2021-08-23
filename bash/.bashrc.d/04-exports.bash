ae82bb0782999f582b290e217e9a4e1206320a0c
Anirudh Oppiliappan <x@icyphox.sh>
2021-08-23 11:01:43 +0530

    GOPROXY=direct, PATH cleanup

--- bash/.bashrc.d/04-exports.bash
+++ bash/.bashrc.d/04-exports.bash
@@ -1,11 +1,16 @@
+# PATH mods
+export PATH=$HOME/bin:$PATH
+export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
+export PATH=$PATH:/usr/local/pgsql/bin
+
 # export thingys
 export EDITOR=/usr/local/bin/nvim
 export SSH_KEY_PATH="~/.ssh/id_rsa"
-export PATH=$HOME/bin:$PATH
 export GPG_TTY=$(tty)
 export INPUTRC=~/.inputrc
-#export PATH=$PATH:$HOME/leet/Nim/bin
-export PATH=$PATH:$HOME/.nimble/bin:$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:/usr/local/pgsql/bin
+export PATH=$PATH:$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:/usr/local/pgsql/bin
 export PW_DIR=~/.pw
 export PW_KEY=x@icyphox.sh
 export _Z_CMD="n"
@@ -13,13 +18,13 @@ export MOZ_ACCELERATED=1
 export CFLAGS="-O3 -pipe -march=native"
 export CXXFLAGS="-O3 -pipe -march=native"
 export MAKEFLAGS="-j4"
+# not this time elgoog
+export GOPROXY=direct
 
 # gpg-agent
-
 [[ -f "$HOME/.gpg-agent-info" ]] && {
     . "$HOME/.gpg-agent-info"
     export GPG_AGENT_INFO
     export SSH_AUTH_SOCK
     export SSH_AGENT_PID
 }
-
