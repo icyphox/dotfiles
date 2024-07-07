929099851811741a8f72cb2d0d5ca29c56a34c27
Anirudh Oppiliappan <x@icyphox.sh>
2024-07-07 10:33:59 +0300

    nix/denna: k3s: you do need the flags

--- hosts/denna/configuration.nix
+++ hosts/denna/configuration.nix
@@ -81,9 +81,10 @@
     # nix-snapshotter.enable = true;
   };
 
-  services.k3s = {
+  services.k3s = let address = "100.77.4.74"; in {
     enable = true;
     role = "agent";
+    extraFlags = "--node-ip=${address} --node-external-ip=${address}";
     serverAddr = "https://sini:6443";
     tokenFile = "/var/lib/rancher/k3s/token";
   };
