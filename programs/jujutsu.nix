c43799ccfd2d0289da770f77df256a6950d113b3
Anirudh Oppiliappan <x@icyphox.sh>
2025-05-21 11:00:00 +0300

    programs/jujutsu: new config
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- programs/jujutsu.nix
+++ programs/jujutsu.nix
@@ -8,51 +8,144 @@
     settings = {
       user = {
         name = "Anirudh Oppiliappan";
-        email = "x@icyphox.sh";
+        email = "anirudh@tangled.sh";
       };
-
+      ui = {
+        # paginate = "never";
+        default-command = "log";
+        pager = "${pkgs.delta}/bin/delta";
+        diff.format = "git";
+        graph.style = "curved";
+        should-sign-off = true;
+      };
       signing = {
         behavior = "own";
         backend = "ssh";
         key = "~/.ssh/id_ed25519.pub";
       };
 
-      git.sign-on-push = true;
-      git.write-change-id-header = true;
-
+      colors = {
+        commit_id = "magenta";
+        change_id = "cyan";
+        "working_copy empty" = "green";
+        "working_copy placeholder" = "red";
+        "working_copy description placeholder" = "yellow";
+        "working_copy empty description placeholder" = "green";
+        prefix = {
+          bold = true;
+          fg = "cyan";
+        };
+        rest = {
+          bold = false;
+          fg = "bright black";
+        };
+        "node elided" = "yellow";
+        "node working_copy" = "green";
+        "node conflict" = "red";
+        "node immutable" = "red";
+        "node normal" = { bold = false; };
+        "node" = { bold = false; };
+      };
+      git = {
+        sign-on-push = true;
+        write-change-id-header = true;
+      };
       aliases = {
+        d = [ "diff" ];
+        l = [ "log" ];
+        ll = [ "log" "-r" ".." ];
         tug = [ "bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-" ];
       };
-
-      ui.paginate = "never";
-      ui.default-command = [ "log" "-n" "10" ];
+      revsets = {
+        log = "current_work";
+      };
+      revset-aliases = {
+        "stack()" = "ancestors(reachable(@, mutable()), 2)";
+        "stack(x)" = "ancestors(reachable(x, mutable()), 2)";
+        "stack(x, n)" = "ancestors(reachable(x, mutable()), n)";
+        "current_work" = "trunk()..@ | @..trunk() | trunk() | @:: | fork_point(trunk() | @)";
+      };
       template-aliases = {
-        custom_log_compact = ''
+        "format_short_id(id)" = "id.shortest()";
+        "abbreviate_timestamp_suffix(s, suffix, abbr)" = ''
+          if(
+              s.ends_with(suffix),
+              s.remove_suffix(suffix) ++ label("timestamp", abbr)
+          )
+        '';
+        "abbreviate_relative_timestamp(s)" = ''
+          coalesce(
+              abbreviate_timestamp_suffix(s, " millisecond", "ms"),
+              abbreviate_timestamp_suffix(s, " second", "s"),
+              abbreviate_timestamp_suffix(s, " minute", "m"),
+              abbreviate_timestamp_suffix(s, " hour", "h"),
+              abbreviate_timestamp_suffix(s, " day", "d"),
+              abbreviate_timestamp_suffix(s, " week", "w"),
+              abbreviate_timestamp_suffix(s, " month", "mo"),
+              abbreviate_timestamp_suffix(s, " year", "y"),
+              s
+          )
+        '';
+        "format_timestamp(timestamp)" = ''
+          coalesce(
+              if(timestamp.after("1 minute ago"), label("timestamp", "<=1m")),
+              abbreviate_relative_timestamp(timestamp.ago().remove_suffix(' ago').remove_suffix('s'))
+          )
+        '';
+      };
+      templates = {
+        log = ''
           if(root,
             format_root_commit(self),
             label(if(current_working_copy, "working_copy"),
               concat(
                 separate(" ",
-                  format_short_change_id_with_hidden_and_divergent_info(self),
-                  author.name(),
-                  bookmarks,
-                  tags,
-                  working_copies,
-                  if(conflict, label("conflict", "conflict")),
+                  pad_end(4, format_short_change_id_with_hidden_and_divergent_info(self)),
                   if(empty, label("empty", "(empty)")),
                   if(description,
                     description.first_line(),
                     label(if(empty, "empty"), description_placeholder),
                   ),
+                  bookmarks,
+                  tags,
+                  working_copies,
+                  if(git_head, label("git_head", "HEAD")),
+                  if(conflict, label("conflict", "conflict")),
+                  if(config("ui.show-cryptographic-signatures").as_boolean(),
+                    format_short_cryptographic_signature(signature)),
+                  format_timestamp(commit_timestamp(self)),
                 ) ++ "\n",
               ),
             )
           )
         '';
-      };
+        log_node = ''
+          label("node",
+            coalesce(
+              if(!self, label("elided", "~")),
+              if(current_working_copy, label("working_copy", "@")),
+              if(conflict, label("conflict", "×")),
 
-      templates = {
-        log = "custom_log_compact";
+              if(immutable, label("immutable", "*")),
+              label("normal", "·")
+            )
+          )
+        '';
+        draft_commit_description = ''
+          concat(
+            coalesce(description, default_commit_description, "\n"),
+            if(
+              config("ui.should-sign-off").as_boolean() && !description.contains("Signed-off-by: " ++ author.name()),
+              "\nSigned-off-by: " ++ author.name() ++ " <" ++ author.email() ++ ">",
+            ),
+            surround(
+              "\nJJ: This commit contains the following changes:\n", "",
+              indent("JJ:     ", diff.stat(72)),
+            ),
+            "\nJJ: ignore-rest\n",
+            diff.git(),
+          )
+        '';
       };
     };
   };
